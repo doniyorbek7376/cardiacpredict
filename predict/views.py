e19b18e639d299a3fac043751c822f310e1ca28f
Doniyorbek Urinboyev <doniyorbek7376@gmail.com>
2023-04-06 20:34:52 +0500

    css

--- predict/views.py
+++ predict/views.py
@@ -8,25 +8,25 @@ def home(request):
     if request.method == 'POST':
         form = UserInputForm(request.POST)
         if form.is_valid():
-            user_data = {
-                'male': 1 if form.cleaned_data['sex'] == 'Male' else 0,
-                'age': form.cleaned_data['age'],
-                'currentSmoker': form.cleaned_data['current_smoker'],
-                'cigsPerDay': form.cleaned_data['cigs_per_day'],
-                'BPMeds': form.cleaned_data['bp_meds'],
-                'prevalentStroke': form.cleaned_data['prevalent_stroke'],
-                'prevalentHyp': form.cleaned_data['prevalent_hyp'],
-                'diabetes': form.cleaned_data['diabetes'],
-                'totChol': form.cleaned_data['tot_chol'],
-                'sysBP': form.cleaned_data['sys_bp'],
-                'diaBP': form.cleaned_data['dia_bp'],
-                'BMI': form.cleaned_data['bmi'],
-                'heartRate': form.cleaned_data['heart_rate'],
-                'glucose': form.cleaned_data['glucose']
-            }
+            user_data = [
+                1 if form.cleaned_data['sex'] == 'Male' else 0,
+                form.cleaned_data['age'],
+                form.cleaned_data['current_smoker'],
+                form.cleaned_data['cigs_per_day'],
+                form.cleaned_data['bp_meds'],
+                form.cleaned_data['prevalent_stroke'],
+                form.cleaned_data['prevalent_hyp'],
+                form.cleaned_data['diabetes'],
+                form.cleaned_data['tot_chol'],
+                form.cleaned_data['sys_bp'],
+                form.cleaned_data['dia_bp'],
+                form.cleaned_data['bmi'],
+                form.cleaned_data['heart_rate'],
+                form.cleaned_data['glucose']
+            ]
 
             prediction_model = PredictionModel.objects.get(name='framingham')
-            result = prediction_model.predict([[value for value in user_data.values()]])
-            context['prediction'] = result
+            result = prediction_model.predict([user_data])
+            context['prediction'] = result[0] * 100
     return render(request, 'home.html', context)
 
